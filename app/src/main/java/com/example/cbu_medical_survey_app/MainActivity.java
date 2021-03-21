39c30cef201dac8e31ee2638e74c1ce659c2615e
GGE2 <dono3107@naver.com>
2021-03-21 18:18:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/cbu_medical_survey_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cbu_medical_survey_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page_portrait);
+    }
+}
