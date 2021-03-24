02d701ed13afab2dd4a5b2f4d53ecbfac2155adf
kimjihun <dono3107@naver.com>
2021-03-24 22:18:31 +0900

    apache poi 4.0.1 구동

--- app/src/main/java/com/example/cbu_medical_survey_app/SubActivity.java
+++ app/src/main/java/com/example/cbu_medical_survey_app/SubActivity.java
@@ -1,8 +1,12 @@
 package com.example.cbu_medical_survey_app;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -35,5 +39,21 @@ public class SubActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.survey_content,fragment).commit();
 
+    }
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        // 키보드 바깥 클릭 시 키보드 닫는 로직
+        View v = getCurrentFocus();
+
+        if(v != null && (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_MOVE) && v instanceof EditText && !v.getClass().getName().startsWith("android.webkit.")){
+            int[] scrcoords = new int[2];
+            v.getLocationOnScreen(scrcoords);
+            float x = event.getRawX() + v.getLeft() - scrcoords[0];
+            float y = event.getRawY() + v.getTop() - scrcoords[1];
+
+            if(x < v.getLeft() || x > v.getRight() || y < v.getTop() || y > v.getBottom())
+                ((InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((this.getWindow().getDecorView().getApplicationWindowToken()), 0);
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
