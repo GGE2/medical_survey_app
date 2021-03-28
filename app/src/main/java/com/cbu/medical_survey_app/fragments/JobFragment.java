9a9e0dc85ab895431d97741555e012bcf452ffeb
leejaeseung <wasd222@naver.com>
2021-03-28 14:05:35 +0900

    직업사항 버튼 로직 모두 구현

--- app/src/main/java/com/cbu/medical_survey_app/fragments/JobFragment.java
+++ app/src/main/java/com/cbu/medical_survey_app/fragments/JobFragment.java
@@ -18,26 +18,31 @@ import com.cbu.medical_survey_app.R;
 public class JobFragment extends Fragment {
 
     private int radio_checked;
+    private boolean[] production_checked;
+    private int mainjob_checked;
 
     private ButtonListener btl;
-    private Context nowContext;
     private position[] positions;
     private production[] productions;
-//    private RadioButton radio_mainjob_no;
-//    private RadioButton radio_mainjob_yes;
+    private RadioButton radio_mainjob_no;
+    private RadioButton radio_mainjob_yes;
+    private EditText input_mainjob;
+    private EditText input_mainjob_year;
 
     public JobFragment() {
-//        check_productions = new CheckBox[24];
-//        radio_mainjob_no = null;
-//        radio_mainjob_yes = null;
+//        positions = new position[11];
+//        productions = new production[24];
+//        radio_checked = -1;
+//        production_checked = new boolean[24];
     }
 
     public JobFragment(Context context) {
         btl = new ButtonListener(context);
-        nowContext = context;
         positions = new position[11];
         productions = new production[24];
         radio_checked = -1;
+        production_checked = new boolean[24];
+        mainjob_checked = -1;
     }
 
     @Override
@@ -61,8 +66,6 @@ public class JobFragment extends Fragment {
             EditText input_position = vg.findViewById(getResId(vg, "input_position_" + (posID + 1)));
             EditText input_position_year = vg.findViewById(getResId(vg, "input_position_year_" + (posID + 1)));
 
-
-
             positions[posID] = new position(radio_position, input_position, input_position_year, posID);
         }
 
@@ -70,17 +73,53 @@ public class JobFragment extends Fragment {
             CheckBox check_production = vg.findViewById(getResId(vg, "check_production_" + (proID + 1)));
             EditText input_production_year = vg.findViewById(getResId(vg, "input_production_year_" + (proID + 1)));
 
-
-
-            productions[proID] = new production(check_production, input_production_year, proID);
+            if(proID < 23)
+                productions[proID] = new production(check_production, input_production_year, proID);
+            else {
+                EditText input_production_other = vg.findViewById(R.id.input_production_other);
+                productions[proID] = new production(check_production, input_production_other, input_production_year, proID);
+            }
         }
 
+        radio_mainjob_no = vg.findViewById(R.id.radio_mainjob_no);
+        radio_mainjob_yes = vg.findViewById(R.id.radio_mainjob_yes);
+
+        input_mainjob = vg.findViewById(R.id.input_mainjob);
+        input_mainjob_year = vg.findViewById(R.id.input_mainjob_year);
+
+        radio_mainjob_no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainjob_checked = 0;
+                input_mainjob.setFocusable(true);
+                input_mainjob.setFocusableInTouchMode(true);
+                input_mainjob.setTextColor(getResources().getColor(R.color.text_black));
+
+                input_mainjob_year.setFocusable(true);
+                input_mainjob_year.setFocusableInTouchMode(true);
+                input_mainjob_year.setTextColor(getResources().getColor(R.color.text_black));
+            }
+        });
+
+        radio_mainjob_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainjob_checked = 1;
+                input_mainjob.setFocusable(false);
+                input_mainjob.setClickable(false);
+                input_mainjob.setTextColor(getResources().getColor(R.color.text_gray));
+
+                input_mainjob_year.setFocusable(false);
+                input_mainjob_year.setClickable(false);
+                input_mainjob_year.setTextColor(getResources().getColor(R.color.text_gray));
+            }
+        });
 
     }
 
     // 문자열 값으로 id를 얻어옴
     private int getResId(ViewGroup vg, String id) {
-        int getID = vg.getResources().getIdentifier(id, "id", nowContext.getPackageName());
+        int getID = vg.getResources().getIdentifier(id, "id", getContext().getPackageName());
         return getID;
     }
 
@@ -108,12 +147,12 @@ public class JobFragment extends Fragment {
                         }
                         radio_checked = index;
                         enableInputs();
-                        System.out.println("여기");
                     }
                 }
             });
         }
 
+        // 현재 줄 비활성화
         private void disableInputs() {
             input.setFocusable(false);
             input.setClickable(false);
@@ -126,6 +165,7 @@ public class JobFragment extends Fragment {
             radio_bt.setChecked(false);
         }
 
+        // 현재 줄 활성화
         private void enableInputs() {
             input.setFocusableInTouchMode(true);
             input.setFocusable(true);
@@ -142,13 +182,69 @@ public class JobFragment extends Fragment {
     private class production {
 
         protected CheckBox check_bt;
+        protected EditText input_other;
         protected EditText input_year;
         protected int index;
 
         public production(CheckBox rb, EditText ipt_y, int idx) {
             check_bt = rb;
+            input_other = null;
             input_year = ipt_y;
             index = idx;
+
+            disableInputs();
+            setBtListener();
+        }
+
+        public production(CheckBox rb, EditText ipt_other, EditText ipt_y, int idx) {
+            check_bt = rb;
+            input_other = ipt_other;
+            input_year = ipt_y;
+            index = idx;
+
+            disableInputs();
+            setBtListener();
+        }
+
+        private void setBtListener() {
+            check_bt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(production_checked[index]){
+                        disableInputs();
+                    }
+                    else{
+                        enableInputs();
+                    }
+                    production_checked[index] = !production_checked[index];
+                }
+            });
+        }
+
+        // 현재 줄 비활성화
+        private void disableInputs() {
+            if(index == 23) {
+                input_other.setFocusable(false);
+                input_other.setClickable(false);
+                input_other.setTextColor(getResources().getColor(R.color.text_gray));
+            }
+
+            input_year.setFocusable(false);
+            input_year.setClickable(false);
+            input_year.setTextColor(getResources().getColor(R.color.text_gray));
+        }
+
+        // 현재 줄 활성화
+        private void enableInputs() {
+            if(index == 23) {
+                input_other.setFocusableInTouchMode(true);
+                input_other.setFocusable(true);
+                input_other.setTextColor(getResources().getColor(R.color.text_black));
+            }
+
+            input_year.setFocusableInTouchMode(true);
+            input_year.setFocusable(true);
+            input_year.setTextColor(getResources().getColor(R.color.text_black));
         }
     }
 
